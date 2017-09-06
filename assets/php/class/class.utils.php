445ead0cb68ccd188eb10b947893b63917468f08
GuilhermeMartin <GuilhermeMartin@users.noreply.github.com>
2017-09-06 09:50:29 -0300

    INDICADORES: Somente usuários com acesso ao painel no banco de dados conseguirão abrir o mesmo através de um caminho URL.

--- assets/php/class/class.utils.php
+++ assets/php/class/class.utils.php
@@ -29,8 +29,15 @@ function menu_dados()
 //Busca o painel BI que deverá exibir na página
 function pbi($pbi_id)
 {
+  $pbi_id = base64_decode($pbi_id);
+  //Verifica se o usuário possui acesso ao painel da INTRANET
+  $acesso = acesso_indicadores($pbi_id);
+  //Se o usuário NÃO possui acesso ao painel da INTRANET
+  if($acesso == 'N'){
+    $link_aux = 'http://intranet.aniger/erro_acesso.html';
+  } else {
     $arquivo = fopen ('http://intranet.aniger/paineis/Modelos.txt', 'r');
-    for ($i=1; $i <= (base64_decode($pbi_id) - 1); $i++) { 
+    for ($i=1; $i <= ($pbi_id - 1); $i++) { 
       fgets($arquivo, 2000);
     };
     $link = fgets($arquivo, 2000); 
@@ -44,18 +51,48 @@ function pbi($pbi_id)
       }  
     }
     fclose($arquivo);
-    $retorno = '<iframe width="100%" height="650" src="'.$link_aux.'"></iframe>';
-    return $retorno;
+  }
+  $retorno = '<iframe width="100%" height="650" src="'.$link_aux.'"></iframe>';
+  return $retorno;
 }
+
+//Verifica se o usuário possui acesso aos indicadores da INTRANET
+function acesso_indicadores($id_painel)
+{
+  //Abre conexão com o banco de dados
+  $host="10.0.0.2";
+  $service="//10.0.0.2:1521/orcl";
+  $id=$_SESSION['usuarioId'];
+  $conn= new \PDO("oci:host=$host;dbname=$service","INTRANET","ifnefy6b9");
+  //Se for para verificar acesso a um painel em específico da INTRANET
+  if ($id_painel == 0) {
+    //Verifica se o usuário possui acesso a um painel determinado da INTRANET
+    $query01 = "SELECT * FROM in_usuarios_paineis WHERE id_usu = :idUSU";
+    $stmt01 = $conn->prepare($query01);
+    $stmt01->bindValue(':idUSU',$id);
+    $stmt01->execute();
+    $result01=$stmt01->fetch(PDO::FETCH_ASSOC);
+  } else {
+    //Verifica se o usuário possui acesso aos indicadores da INTRANET
+    $query01 = "SELECT * FROM in_usuarios_paineis WHERE id_usu = :idUSU AND id_painel = :idPAINEL";
+    $stmt01 = $conn->prepare($query01);
+    $stmt01->bindValue(':idUSU',$id);
+    $stmt01->bindValue(':idPAINEL',$id_painel);
+    $stmt01->execute();
+    $result01=$stmt01->fetch(PDO::FETCH_ASSOC);
+  }
+  //Se o usuário NÃO possui acesso aos indicadores da INTRANET
+  if(empty($result01)){
+    $acesso = 'N';
+  } else {
+    $acesso = 'S';
+  }
+  return $acesso;
+}  
 
 //Exibe o menu lateral das páginas WEB
 function exibe_menu_lateral($id_foco)
 {
-    //Abre conexão com o banco de dados
-    $host="10.0.0.2";
-    $service="//10.0.0.2:1521/orcl";
-    $id=$_SESSION['usuarioId'];
-    $conn= new \PDO("oci:host=$host;dbname=$service","INTRANET","ifnefy6b9");
     //Insere o menu lateral
     echo '<p class="menu-title sm">MENU <span class="pull-right"><a href="javascript:;"><i class="material-icons">refresh</i></a></span></p>
           <ul>';
@@ -122,14 +159,10 @@ function exibe_menu_lateral($id_foco)
                 <a href="uteis.php"><i class="fa fa-external-link" title="&uacute;teis"></i> <span class="title">Links &uacute;teis</span></a>
               </li>';
     }
-    //Verifica se o usuário possui acesso aos indicadores da INTRANET
-    $query01 = "SELECT * FROM in_usuarios_paineis WHERE id_usu = :idUSU";
-    $stmt01 = $conn->prepare($query01);
-    $stmt01->bindValue(':idUSU',$id);
-    $stmt01->execute();
-    $result01=$stmt01->fetch(PDO::FETCH_ASSOC);
-    //Se o usuário possui acesso aos indicadores da INTRANET
-    if(! empty($result01)){
+    //Verifica se o usuário possui acesso ao painel da INTRANET
+    $acesso = acesso_indicadores(0);
+    //Se o usuário possui acesso ao painel da INTRANET
+    if($acesso == 'S'){
       if($id_foco == "indicadores.php"){
         echo '  <li class="start active"> 
                   <a href="indicadores.php"><i class="fa fa-bar-chart" title="Indicadores"></i> <span class="title">Indicadores</span></a>
